78037d08a3226a258a13f65cd23a9a27eaff2bdc
ko_ri <63943931+nshciei45@users.noreply.github.com>
2023-01-16 14:42:13 +0630

    login page design template

---
+++ login-temp.php
@@ -0,0 +1,18 @@
+<div className="Login" class='container'>
+                <div class="bg-light p-5 mt-5 col-md-6 container border border-2 border-dark-subtle rounded-4 bg-tertiary ">
+                    <form>
+                        <div class='form-group form-floating mb-3'>
+                            <input type='email' class='form-control' id='email' placeholder='Email address' />
+                            <label for='email'>Email address</label>
+                        </div>
+                        <div class='form-group form-floating mb-3'>
+                            <input type='password' class='form-control' id='password' placeholder='Password' />
+                            <label for='password'>Password</label>
+                        </div>
+                        <div class='d-flex flex-column align-items-end'>
+                            <button type='submit' class='btn btn-primary'>Login</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
